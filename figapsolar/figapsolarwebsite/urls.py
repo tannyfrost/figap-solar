050f02ff6e22388aff825b893e7493e136849062
Tani T <tani.abereoje@gmail.com>
2025-07-21 16:30:19 +0100

    Fixed the carousel, made finishing touches to the website

--- figapsolar/figapsolarwebsite/urls.py
+++ figapsolar/figapsolarwebsite/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('test/', views.secondpage)
+    path('', views.index, name='home'),
+    path('test/', views.secondpage, name ='test'),
+    path('about/', views.about, name = 'about'),
+    path('service/', views.service, name = 'service'),
+    path('project/', views.project, name = 'project'),
+    path('contact/', views.contact, name = 'contact'),
 ]
--- figapsolar/figapsolarwebsite/views.py
+++ figapsolar/figapsolarwebsite/views.py
@@ -8,3 +8,15 @@ def index(request):
 
 def secondpage(request):
     return render(request, 'figapsolarwebsite/i.html')
+
+def about(request):
+    return render(request, 'figapsolarwebsite/about.html')
+
+def service(request):
+    return render(request, 'figapsolarwebsite/service.html')
+
+def project(request):
+    return render(request, 'figapsolarwebsite/project.html')
+
+def contact(request):
+    return render(request, 'figapsolarwebsite/contact.html')
